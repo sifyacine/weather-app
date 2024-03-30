7f8d4f3f802ff9884fdef0c2ff014778b81e3e75
YCN <89133149+sifyacine@users.noreply.github.com>
2024-03-30 13:38:03 +0100

    start of the app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp( WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
